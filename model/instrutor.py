a3c429348da36d82c6138b6f06fc9b799f0ab7ee
PedroCarniel <pedrohcastamancarniel@gmail.com>
2026-06-08 02:19:43 -0300

    Fix: Adicionado a classe de dominio Instrutor na pasta model

---
+++ model/instrutor.py
@@ -0,0 +1,6 @@
+# model/instrutor.py
+class Instrutor:
+    def __init__(self, id_instrutor=None, nome=None, cref=None):
+        self.id = id_instrutor
+        self.nome = nome
+        self.cref = cref
